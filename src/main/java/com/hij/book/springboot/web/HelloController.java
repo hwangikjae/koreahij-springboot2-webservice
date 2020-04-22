e5b772ec850dc3748ea60901f74cdb0242bfb652
hwangikjae <hwangikjae@nate.com>
2020-04-22 19:38:55 +0900

    20200422-p.77까지 2챕터 끝

--- src/main/java/com/hij/book/springboot/web/HelloController.java
+++ src/main/java/com/hij/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.hij.book.springboot.web;
 
+import com.hij.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController//컨트롤러를 json을 반환하는 컨트롤러로 만들어 준다.
@@ -11,5 +13,11 @@ public class HelloController {
     //이제 이 프로젝트는 /hello로 요청이 오면 문자열 hello를 반환하는 기능을 가진다.
     public String hello(){
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name
+                                    , @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
     }
 }
